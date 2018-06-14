303f0323de4a787d4437e3833cf000edbb9aa562
zeper <zeperx@gmail.com>
2018-06-14 22:10:44 +0900

    Update makecscope.sh
    add -b option

--- makecscope.sh
+++ makecscope.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm cscope.files cscope.out
 find . \( -name '*.c' -o -name '*.cpp"' -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name '*.S' \) -print >> cscope.files
-cscope -i cscope.files
+cscope -b -i cscope.files
